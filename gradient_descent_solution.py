296bb36a00b52464def22fad62fe6fb5e45dd886
Fudayl Çavuş <fudaylcavus@gmai.com>
2021-11-04 01:32:31 +0300

    clean up & compatibility improvement

--- gradient_descent_solution.py
+++ gradient_descent_solution.py
@@ -1,3 +1,13 @@
+"""
+Owner : Fudayl Ikbal Cavus 
+Project : Linear Regression 
+Data Set: StreetEasy Rental Real Estate Market Reports
+Data Set Source: https://github.com/Codecademy/datasets/tree/master/streeteasy
+Date : 03 November 2021
+Time : 16:22
+Copyright (C) belong to ComVIS Lab/NET
+"""
+
 from utils import (
     x_train,
     x_test,
@@ -17,84 +27,66 @@ class Regression:
         self.coef_ = None
         self.intercept_ = None
         self.mean = None
-        self.sigma = None
+        self.std = None
         self.N = None
 
     def z_score_fit(self, xs: np.ndarray):
         self.mean = xs.mean()
-        self.sigma = np.std(xs)
+        self.std = np.std(xs)
         return self
 
     def z_score_transform(self, xs: np.ndarray):
-        return (xs - self.mean) / self.sigma
+        return (xs - self.mean) / self.std
 
     def score(self, xs, ys):
         # 1 - (rss/tss) gives the accuracy
-        xs = self.z_score_transform(xs)
-        y_predict = np.dot(self.coef_, xs.T) + self.intercept_
+        y_predict = self.predict(xs) 
         RSS = ((ys - y_predict) ** 2).sum()
+        print(RSS)
         TSS = ((ys - ys.mean()) ** 2).sum()
         return 1 - (RSS / TSS)
 
     def predict(self, xs):
+        xs, ys = self.validate(xs)
         xs = self.z_score_transform(xs)
         return xs.dot(self.coef_) + self.intercept_
+    
+    def validate(self, xs, ys=None):
+        if type(xs) != np.ndarray:
+            xs = np.array(xs, dtype=float)
+        if type(ys) != np.ndarray:
+            ys = np.array(ys, dtype=float)
+        if xs.ndim == 1:
+            xs = xs.reshape(-1, 1)
+        return xs, ys
 
 
-class SimpleRegression(Regression):
-    def __init__(self, epoch, learning_rate, degree):
-        super().__init__(epoch, learning_rate, degree)
 
-    def fit(self, xs, ys):
-        # degree + 1 cause of intercept (w0 or bias)
-        ws = np.zeros(self.degree + 1, dtype=float)
-
-        # Cause of it's polynomial for each X value we will use x to the power of [0, 1, ,2, 3, ,5.. degree+1]
-        # with respect to index of W
-        power_indexes = [np.repeat(i, len(xs)) for i in range(len(ws))]
-
-        for _ in range(self.epoch):
-            y_predict = np.dot(ws, np.power(xs, power_indexes))
-            gradient = (-2 / len(xs)) * np.dot((ys - y_predict), np.power(xs, power_indexes).transpose())
-            ws = ws - (gradient * self.learning_rate)
-        self.coef_ = ws[1:]
-        self.intercept_ = ws[0]
-
-
-class MultipleRegression(Regression):
-    def __init__(self, epoch, learning_rate, penalty):
-        # For now, hardcoded degree to 1 to only calculate multiple linear regression
+class LinearRegression(Regression):
+    def __init__(self, epoch=1000, learning_rate=0.03, penalty=0):
         super().__init__(epoch, learning_rate, 1, penalty)
 
-    def fit(self, x_values, ys, how='ridge'):
-        print(x_values.shape)
+    def fit(self, x_values, y_values):
+        x_values, y_values = self.validate(x_values, y_values)
         self.N, self.degree = x_values.shape
         ws = np.zeros(self.degree + 1, dtype=float)
         xs = self.z_score_fit(x_values).z_score_transform(x_values)
 
         # adding every first indexes a 1 for intercept (w0 or b)
         xs = np.insert(xs, 0, 1, axis=1)
-        ys = ys.reshape(-1)
 
         for _ in range(self.epoch):
             y_predict = xs.dot(ws)
-            loss = (ys - y_predict)
-            #
+            loss = (y_values - y_predict)
+            #Ridge Regression
             gradient = (-2 * (xs.T.dot(loss) + (2 * ws * self.penalty))) / self.N
             ws = ws - (gradient * self.learning_rate)
         self.coef_ = ws[1:]
         self.intercept_ = ws[0]
 
-    def plot_cost_by_epoch(self):
-        plt.plot(self.cost_log)
-        plt.xlabel('Epoch')
-        plt.ylabel('RSS')
-        plt.title('class')
-        plt.show()
-
 
 if __name__ == '__main__':
-    model = MultipleRegression(2000, 0.03, 1)
+    model = LinearRegression()
     model.fit(x_train, y_train)
     print(model.coef_, model.intercept_)
 
@@ -105,7 +97,7 @@ if __name__ == '__main__':
     print("Test Score: ", round(test_score, 2))
 
     # To clearly see y_predict and y_real with linear plot
-    plt.plot(range(20000), range(20000), c='black')
+    plt.plot(range(5), range(5), c='black')
     y_predict = model.predict(x_train)
     plt.scatter(y_train, y_predict, alpha=0.3)
     plt.xlabel('Y Real')
--- utils.py
+++ utils.py
@@ -1,74 +1,14 @@
 import time
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
-# Polynomial dots generation
-trX = np.linspace(-1, 1, 101)
-num_coeffs = 1
-trY_coeffs = [ 2]
-trY = 0
-for i in range(num_coeffs):
-    trY += trY_coeffs[i] * np.power(trX, i)
-    trY += np.random.randn(*trX.shape) * .5
-
-plt.scatter(trX, trY)
-plt.show()
-xs = trX
-ys = trY
-
+import matplotlib.pyplot as plt
 house_data = pd.read_csv('streeteasy.csv')
-# x_all = house_data[['bedrooms', 'bathrooms', 'size_sqft', 'min_to_subway']]
-x_all = house_data[['size_sqft', 'bedrooms', 'bathrooms']]
-y_all = house_data.rent  # thing we want to predict
+x_all = house_data[['size_sqft', 'min_to_subway']]
+y_all = house_data.rent  
 
-x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, train_size=50, random_state=0)
-x_train, x_test, y_train, y_test = np.array(x_train), np.array(x_test), np.array(y_train), np.array(y_test)
-# x_train = xs.reshape(-1, 1)
-# y_train = ys.reshape(-1, 1)
-# x_train = x_train[:5]
-# y_train = y_train[:5]
-
-
-def plot_polynom_and_points(coeffs, xs, ys):
-    plt.plot(xs, xs.dot(coeffs), c='black')
-    print(xs[:, 1])
-    print(ys)
-    plt.scatter(xs[:, 1], ys)
-
-    plt.show()
-
-
-def plot_line_and_points(b, w, x_values, y_values):
-    # since 'plot' method automatically connects dots to make a line
-    # selecting minimum and maximum point would include all other points
-    # cause of it's linear
-    min_x = min(x_values)
-    max_x = max(x_values)
-    xs = [min_x, max_x]
-    ys = [b + w * x for x in xs]
-    plt.plot(xs, ys)
-    plt.plot(x_values, y_values, 'o')
-    plt.show()
-
-
-def plot_3d_points(x_values, y_values, z_values, z_prediction):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(x_values, y_values, z_values, c='red')
-
-    ax.plot_trisurf(x_values, y_values, z_prediction, antialiased=True, linewidth=0)
-    ax.view_init(10, 40)
-    plt.show()
-
-
-def find_rss(coeffs, x_real, y_real):
-    cost = np.sum(((x_real.dot(coeffs) - y_real) ** 2) / (2 * len(y_real)))
-    return cost
-    # rss = ((y_real - (np.dot(coeffs,  x_real.T) + intercept))**2).sum()
-    # return rss
-
+x_train, x_test, y_train, y_test = train_test_split(x_all, y_all)
+    
 
 def get_execution_time_by_running(func, **kwargs):
     start = time.time()
